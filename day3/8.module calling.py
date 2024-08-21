8fa8815d89aeefda43f0fffd3397727dd6183bf2
VENKITEELA KOUSHIK NAIDU <122259567+Koushik13579@users.noreply.github.com>
2024-08-21 10:25:50 +0530

    Add files via upload

---
+++ day3/8.module calling.py
@@ -0,0 +1,5 @@
+import calc
+print("divison result=",calc.div(4,2))
+print("Multiplication result=",calc.mul(4,2))
+print("Addition result=",calc.add(4,2))
+print("Subtraction result=",calc.sub(4,2))
